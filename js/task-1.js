8bb1c057d94cffe20fa4b0a2aa0ee1b2184a7284
b-kievskiy <60388615+b-kievskiy@users.noreply.github.com>
2020-03-03 10:01:56 +0200

    first

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+// do {
+//     console.log("hello world");
+// } while (i < 20);
+
+// while (i < 20) {
+//     console.log("hello world");
+// }
+
+// console.log("after while");
+
+for (let i = 0; i < 10; i = i +1) {
+    console.log("hello world", i);
+}
+
